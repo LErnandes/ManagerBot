992711fd06397a6462126cfb1df708ca5a8ee06e
unknown <luis78270@gmail.com>
2021-03-02 02:51:02 -0300

    Adding error handler

--- index.js
+++ index.js
@@ -5,24 +5,27 @@ require('dotenv').config()
 const js_exec = require('./features/js_exec')
 
 
-routes = {
-    "js": "js_exec"
-}
-
-
 client.on('ready', () => {
     console.log(`Entrando como ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
     var message = msg.content;
-    if (message.startsWith('-mb ')) {
-        message = message.replace('-mb ', '')
-        args = message.split(' ');
+    try {
+        if (message.startsWith('-mb ')) {
+            message = message.replace('-mb ', '')
+            var args = message.split(' ');
+            var res;
 
-        let res = eval(`${routes[args[0]]}(${args.slice(1, args.lenght).join()})`)
+            if (args[0] == "js") {
+                let res = js_exec(args.slice(1, args.lenght).join(' '))
+            }
 
-        msg.channel.send(res);
+            msg.channel.send(res);
+        }
+    } catch(err) {
+        console.error(err)
+        msg.channel.send(`Ocorreu um erro`);
     }
 });
 
