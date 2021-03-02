9b03dde8cd86595a92a73c50ddc579fa37ba0b6a
unknown <luis78270@gmail.com>
2021-03-02 02:39:56 -0300

    Adding js_exec feature

---
+++ features/js_exec.js
@@ -0,0 +1,5 @@
+function js_exec(js_string) {
+    return eval("js_string")
+}
+
+module.exports = js_exec;
--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 require('dotenv').config()
 
+const js_exec = require('./features/js_exec')
+
+
+routes = {
+    "js": "js_exec"
+}
+
 
 client.on('ready', () => {
     console.log(`Entrando como ${client.user.tag}!`);
@@ -11,8 +18,11 @@ client.on('message', msg => {
     var message = msg.content;
     if (message.startsWith('-mb ')) {
         message = message.replace('-mb ', '')
+        args = message.split(' ');
 
-        msg.channel.send(message)
+        let res = eval(`${routes[args[0]]}(${args.slice(1, args.lenght).join()})`)
+
+        msg.channel.send(res);
     }
 });
 
